1b89620814c5a5ad733fb17789f6753f590bb9d4
Serena <serenalin0420@gmail.com>
2024-09-16 10:57:34 +0800

    setup routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,31 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Post from "./pages/Post";
+import CreatePost from "./pages/CreatePost";
+import Profile from "./pages/Profile/index.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            {/* 只有登入的才能申請課程  path="posts/:id"  */}
+            <Route path="posts" element={<Post />} />
+            {/* 發布貼文isLogin */}
+            <Route path="create-post" element={<CreatePost />} />
+            {/* profile需要做role自己or其他人判斷 */}
+            <Route path="profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 );
