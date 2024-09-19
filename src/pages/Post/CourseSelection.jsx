1f3907e92141e4ffabc3dd9afbc56404ac52736b
Serena <serenalin0420@gmail.com>
2024-09-19 21:42:03 +0800

    show the times that users choose

--- src/pages/Post/CourseSelection.jsx
+++ src/pages/Post/CourseSelection.jsx
@@ -15,12 +15,13 @@ const CourseSelection = ({
   daysOfWeek,
 }) => {
   const [showModal, setShowModal] = useState(false);
-  // 新增一個單獨的狀態來管理 modal 的 timetable
   const [modalState, modalDispatch] = useReducer(reducer, initialState);
   const [selectedCourse, setSelectedCourse] = useState();
   const [selectedCoinCost, setSelectedCoinCost] = useState();
+  const [selectedTimes, setSelectedTimes] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const order = ["試教", "1", "3", "5", "10"];
+  const order = ["體驗", "1", "3", "5", "10"];
   const sortedCourseNum = post.course_num
     ? post.course_num.sort((a, b) => {
         return order.indexOf(a) - order.indexOf(b);
@@ -29,12 +30,12 @@ const CourseSelection = ({
 
   const getDisplayText = (num) => {
     switch (num) {
-      case "試教":
-        return "試教 / 25分鐘";
+      case "體驗":
+        return "體驗 / 25分鐘";
       case "1":
         return "1 次 / 50分鐘";
       case "3":
-        return "3 次 （約2.5小時） ";
+        return "3 次 （約2.5小時）";
       case "5":
         return "5 次 （約6小時）";
       case "10":
@@ -43,11 +44,11 @@ const CourseSelection = ({
         return `${num} 堂 / 未知時間`;
     }
   };
+
   const handleTimeSlotClick = (date, time) => {
     const dateString = formatDate(date, "yyyy-MM-dd");
     const selectedTimesForDate = modalState.selectedTimes[dateString] || {};
 
-    // 檢查該時間段是否已經被選取
     const isSelected = selectedTimesForDate[time];
 
     const updatedTimesForDate = {
@@ -55,13 +56,26 @@ const CourseSelection = ({
       [time]: !isSelected,
     };
 
-    modalDispatch({
-      type: actionTypes.SET_SELECTED_TIMES,
-      payload: {
-        ...modalState.selectedTimes,
-        [dateString]: updatedTimesForDate,
-      },
-    });
+    let newSelectedTimes = !isSelected
+      ? [...selectedTimes, `${dateString} ${time}`]
+      : selectedTimes.filter((t) => t !== `${dateString} ${time}`);
+
+    const maxSelectableTimes =
+      selectedCourse === "體驗" ? 1 : parseInt(selectedCourse, 10);
+    if (newSelectedTimes.length > maxSelectableTimes) {
+      setErrorMessage(`您只能選擇 ${maxSelectableTimes} 個時段`);
+      newSelectedTimes = selectedTimes; // revert to previous state
+    } else {
+      setErrorMessage("");
+      setSelectedTimes(newSelectedTimes);
+      modalDispatch({
+        type: actionTypes.SET_SELECTED_TIMES,
+        payload: {
+          ...modalState.selectedTimes,
+          [dateString]: updatedTimesForDate,
+        },
+      });
+    }
   };
 
   const renderTimeSlots = (day) => {
@@ -73,11 +87,15 @@ const CourseSelection = ({
         {Object.keys(timeSlots).length > 0 ? (
           Object.keys(timeSlots).map((time) => {
             const isAvailable = timeSlots[time];
-            const isSelected = modalState.selectedTimes[dateKey]?.[time]; // 檢查是否被選取
+            const isSelected = modalState.selectedTimes[dateKey]?.[time];
             return (
               <div
                 key={time}
-                className={`mt-1 flex w-full cursor-pointer flex-col px-3 py-1 text-center ${isAvailable ? "font-semibold text-yellow-800" : "text-zinc-400"} ${isSelected ? "bg-yellow-300" : ""}`}
+                className={`mt-1 flex w-full cursor-pointer flex-col px-3 py-1 text-center ${
+                  isAvailable
+                    ? "font-semibold text-yellow-800"
+                    : "text-zinc-400"
+                } ${isSelected ? "bg-yellow-300" : ""}`}
                 onClick={() => isAvailable && handleTimeSlotClick(day, time)}
               >
                 {time}
@@ -92,7 +110,18 @@ const CourseSelection = ({
       </div>
     );
   };
+
   const handleConfirm = async () => {
+    if (
+      (selectedCourse === "體驗" && selectedTimes.length < 1) ||
+      (selectedCourse !== "體驗" &&
+        selectedTimes.length < parseInt(selectedCourse, 10))
+    ) {
+      setErrorMessage(
+        `您需要選擇 ${selectedCourse === "體驗" ? 1 : selectedCourse} 個時段`,
+      );
+      return;
+    }
     try {
       const updatedTimes = Object.keys(modalState.selectedTimes).reduce(
         (acc, date) => {
@@ -100,7 +129,7 @@ const CourseSelection = ({
           const updatedTimesForDate = Object.keys(timesForDate).reduce(
             (timeAcc, time) => {
               if (timesForDate[time]) {
-                timeAcc[time] = false; // 只有選中的時間才更新為 false
+                timeAcc[time] = false;
               }
               return timeAcc;
             },
@@ -113,6 +142,7 @@ const CourseSelection = ({
         },
         {},
       );
+      console.log("updatedTimes", updatedTimes);
 
       await dbApi.updatePost(post.post_id, {
         datetime: {
@@ -121,7 +151,6 @@ const CourseSelection = ({
         },
       });
 
-      // 清空選取的時間
       modalDispatch({
         type: actionTypes.SET_SELECTED_TIMES,
         payload: {},
@@ -133,13 +162,29 @@ const CourseSelection = ({
     }
   };
 
+  const formatSelectedTime = (time) => {
+    const [date, hour] = time.split(" ");
+    const dayOfWeek = new Date(date)
+      .toLocaleDateString("zh-TW", {
+        weekday: "short",
+      })
+      .replace("週", "");
+    const formattedDate = new Date(date).toLocaleDateString("zh-TW", {
+      month: "numeric",
+      day: "numeric",
+    });
+    const startTime = `${hour}:00`;
+    const endTime = `${parseInt(hour, 10) + 1}:50`;
+    return `${formattedDate} (${dayOfWeek})   ${startTime} - ${endTime}`;
+  };
+
   return (
     <div className="mt-8">
       <h1 className="mb-4 text-center text-2xl">上課需求</h1>
       <div className="flex justify-between gap-6">
         {sortedCourseNum.map((num, index) => {
           const coinCost =
-            num === "試教" ? 1 : post.coin_cost * parseInt(num, 10);
+            num === "體驗" ? 1 : post.coin_cost * parseInt(num, 10);
           return (
             <div
               key={index}
@@ -147,7 +192,7 @@ const CourseSelection = ({
               onClick={() => {
                 setShowModal(true);
                 setSelectedCourse(num);
-                setSelectedCoinCost(coinCost); // 設置選取的 coinCost
+                setSelectedCoinCost(coinCost);
               }}
             >
               <img src={coin} alt="coin" className="size-16 object-cover" />
@@ -160,7 +205,7 @@ const CourseSelection = ({
       </div>
       {showModal && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="relative top-16 flex max-h-[calc(100vh-32px)] flex-col overflow-y-auto rounded-md bg-white p-6 shadow-md">
+          <div className="relative top-16 flex max-h-[calc(100vh-32px)] flex-col overflow-y-auto rounded-md bg-white px-8 py-4 shadow-md">
             <div className="flex items-center">
               <div className="m-2.5 h-20 w-20 overflow-hidden rounded-full">
                 <img
@@ -176,23 +221,23 @@ const CourseSelection = ({
                 </h3>
               </div>
               {selectedCourse && (
-                <div className="mb-[2px] ml-auto mr-8 mt-auto text-xl">
+                <div className="mb-[2px] ml-72 mr-8 mt-auto">
                   次數：{selectedCourse}
                 </div>
               )}
-              <div className="mt-auto flex items-center text-xl">
+              <div className="mt-auto flex items-center">
                 獲得：
                 <img
                   src={coin}
                   alt="coin"
                   className="mr-2 size-8 object-cover"
                 />
-                <p className="font-bold text-yellow-900">
+                <p className="text-lg font-bold text-yellow-900">
                   x {selectedCoinCost}
                 </p>
               </div>
               <button
-                className="self-start p-2"
+                className="ml-auto self-start p-2"
                 onClick={() => setShowModal(false)}
               >
                 <svg
@@ -212,7 +257,7 @@ const CourseSelection = ({
               </button>
             </div>
 
-            <div className="mt-2 flex gap-8">
+            <div className="mt-2 flex gap-4">
               <div className="flex flex-col rounded-b-lg shadow-md">
                 <div className="flex h-12 w-full items-center rounded-t-lg bg-zinc-500 px-6 text-xl text-white">
                   學習時間表
@@ -232,13 +277,25 @@ const CourseSelection = ({
                   />
                 </div>
               </div>
-
-              <button
-                className="mt-4 self-end rounded bg-blue-500 px-4 py-2 text-white"
-                onClick={handleConfirm}
-              >
-                確認
-              </button>
+              <div className="flex flex-col items-center">
+                <h3 className="mt-12 text-lg font-semibold">已選擇的時段</h3>
+                <ul>
+                  {selectedTimes.map((time, index) => (
+                    <li key={index}>{formatSelectedTime(time)}</li>
+                  ))}
+                </ul>
+                {errorMessage && (
+                  <div className="mt-4 text-sm text-red-500">
+                    {errorMessage}
+                  </div>
+                )}
+                <button
+                  className="mt-auto self-end rounded-md bg-orange-400 px-4 py-2 text-white"
+                  onClick={handleConfirm}
+                >
+                  確認
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -254,6 +311,7 @@ CourseSelection.propTypes = {
     course_num: PropTypes.arrayOf(PropTypes.string),
     title: PropTypes.string.isRequired,
     datetime: PropTypes.object.isRequired,
+    author_uid: PropTypes.string.isRequired,
   }).isRequired,
   author: PropTypes.shape({
     name: PropTypes.string,
