2450114604ea430e839ec6c4f1c467aff4733647
Serena <serenalin0420@gmail.com>
2024-09-19 14:08:47 +0800

    feat: fetch data and show on Home page

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,9 +1,14 @@
 import PropTypes from "prop-types";
 
-const StarRating = ({ rating }) => {
+const StarRating = ({ rating, size }) => {
   const fullStars = Math.floor(rating); // 計算整數部分
   const hasHalfStar = rating % 1 >= 0.25 && rating % 1 < 0.75; // 判斷是否顯示半顆星
   const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0); // 計算剩餘空星數
+
+  const starStyle = {
+    width: size,
+    height: size,
+  };
 
   return (
     <div className="flex">
@@ -11,9 +16,10 @@ const StarRating = ({ rating }) => {
       {[...Array(fullStars)].map((_, index) => (
         <svg
           key={`full-${index}`}
-          className="h-5 w-5 text-yellow-500"
+          className="size-5 text-yellow-500"
           fill="currentColor"
           viewBox="0 0 20 20"
+          style={starStyle}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.957a1 1 0 00.95.69h4.162c.969 0 1.371 1.24.588 1.81l-3.37 2.448a1 1 0 00-.364 1.118l1.287 3.957c.3.921-.755 1.688-1.54 1.118l-3.37-2.448a1 1 0 00-1.175 0l-3.37 2.448c-.784.57-1.838-.197-1.54-1.118l1.287-3.957a1 1 0 00-.364-1.118L2.05 9.384c-.784-.57-.38-1.81.588-1.81h4.162a1 1 0 00.95-.69l1.286-3.957z" />
         </svg>
@@ -21,7 +27,7 @@ const StarRating = ({ rating }) => {
 
       {/* 半星 */}
       {hasHalfStar && (
-        <svg className="h-5 w-5" viewBox="0 0 20 20">
+        <svg viewBox="0 0 20 20" style={starStyle} className="size-5">
           <defs>
             <linearGradient
               id="halfStarGradient"
@@ -45,9 +51,10 @@ const StarRating = ({ rating }) => {
       {[...Array(emptyStars)].map((_, index) => (
         <svg
           key={`empty-${index}`}
-          className="h-5 w-5 text-gray-200"
+          className="size-5 text-gray-200"
           fill="currentColor"
           viewBox="0 0 20 20"
+          style={starStyle}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.957a1 1 0 00.95.69h4.162c.969 0 1.371 1.24.588 1.81l-3.37 2.448a1 1 0 00-.364 1.118l1.287 3.957c.3.921-.755 1.688-1.54 1.118l-3.37-2.448a1 1 0 00-1.175 0l-3.37 2.448c-.784.57-1.838-.197-1.54-1.118l1.287-3.957a1 1 0 00-.364-1.118L2.05 9.384c-.784-.57-.38-1.81.588-1.81h4.162a1 1 0 00.95-.69l1.286-3.957z" />
         </svg>
@@ -58,6 +65,11 @@ const StarRating = ({ rating }) => {
 
 StarRating.propTypes = {
   rating: PropTypes.number.isRequired,
+  size: PropTypes.string,
 };
+
+// StarRating.defaultProps = {
+//   size: "20px", // 默認尺寸
+// };
 
 export default StarRating;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,21 +1,112 @@
 import SwitchBtn from "../../components/SideBar/SwitchBtn";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ViewContext } from "../../context/viewContext";
+import dbApi from "../../utils/api";
+import StarRating from "../../components/StarRating";
+import coin from "../../components/coin.svg";
 
 function Home() {
   const { isProviderView } = useContext(ViewContext);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const fetchedPosts = await dbApi.getAllPosts();
+        const postsWithAuthors = await Promise.all(
+          fetchedPosts.map(async (post) => {
+            if (post.author_uid) {
+              const authorData = await dbApi.getProfile(post.author_uid);
+              return { ...post, author: authorData };
+            }
+            return post;
+          }),
+        );
+        setPosts(postsWithAuthors);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  const formatDate = (timestamp) => {
+    if (!timestamp) return "";
+    const date = new Date(
+      timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000,
+    );
+    return date.toLocaleDateString();
+  };
+
+  const truncateText = (text, maxLength) => {
+    if (!text) return "";
+    if (text.length <= maxLength) {
+      return text;
+    }
+    return text.slice(0, maxLength) + "...";
+  };
 
   return (
-    <div className="">
+    <div className="mx-8 mt-20 flex">
       <SwitchBtn />
       <div className="mt-4">
         {isProviderView ? (
           <div>
-            <h3 className="text-center">學生的內容</h3>
+            <h3 className="text-center">學生發的文</h3>
+            <button>發布教學</button>
           </div>
         ) : (
-          <div>
-            <h3 className="text-center">老師</h3>
+          <div className="p-4">
+            <button>發起學習</button>
+            <div className="grid grid-cols-3 gap-2">
+              {posts.map((post) => (
+                <div
+                  key={post.id}
+                  className="rounded-lg border border-gray-300 p-4"
+                >
+                  <div className="flex">
+                    <div className="mr-2 size-16 overflow-hidden rounded-full">
+                      <img
+                        src={post.author?.profile_picture}
+                        className="h-full w-full object-cover object-center"
+                        alt="author"
+                      />
+                    </div>
+                    <div className="mt-1">
+                      <h4 className="mb-1 text-lg">{post.author?.name}</h4>
+                      <StarRating
+                        rating={post.author?.review_rating ?? 0}
+                        size="16px"
+                      />
+                    </div>
+                    <p className="ml-auto">{formatDate(post.created_time)}</p>
+                  </div>
+                  <p className="mt-2">{truncateText(post.description, 24)}</p>
+                  <div className="mt-4 flex gap-2">
+                    {post.location?.map((loc, index) => (
+                      <p
+                        key={index}
+                        className="rounded-md bg-slate-200 px-2 py-1 text-sm"
+                      >
+                        {loc}
+                      </p>
+                    ))}
+                  </div>
+                  <div className="mt-4 flex items-center">
+                    <img
+                      src={coin}
+                      alt="coin"
+                      className="mr-1 size-10 object-cover"
+                    />
+                    <p className="text-2xl font-bold text-yellow-900">
+                      {" "}
+                      {post.coin_cost}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         )}
       </div>
