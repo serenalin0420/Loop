016da678a21a6391d3e2d48e823682e72f093b08
Serena <serenalin0420@gmail.com>
2024-09-19 11:51:29 +0800

    feat: add SwitchBtn component

---
+++ src/components/SideBar/SwitchBtn.jsx
@@ -0,0 +1,38 @@
+import tamtam from "./tamtam.svg";
+import { useContext } from "react";
+import { ViewContext } from "../../context/viewContext";
+
+function SwitchBtn() {
+  const { isProviderView, setIsProviderView } = useContext(ViewContext);
+
+  const handleSwitch = (view) => {
+    setIsProviderView(view); // 修改全局狀態
+  };
+
+  return (
+    <div className="flex h-1/6 flex-col items-center rounded-lg px-8 py-5 shadow-md">
+      <h2 className="text-center text-lg font-bold">技能學習區</h2>
+      <img src={tamtam} className="w-44" />
+      <div className="flex w-40 justify-center rounded-full bg-[#BFAA87] p-2">
+        <button
+          className={`rounded-full p-1 px-3 text-white ${
+            isProviderView ? "text-white" : "bg-[#F2EBDF] text-yellow-900"
+          }`}
+          onClick={() => handleSwitch(false)}
+        >
+          找老師
+        </button>
+        <button
+          className={`rounded-full p-1 px-3 text-white ${
+            isProviderView ? "bg-[#F2EBDF] text-yellow-900" : ""
+          }`}
+          onClick={() => handleSwitch(true)}
+        >
+          找學生
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default SwitchBtn;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,26 @@
+import SwitchBtn from "../../components/SideBar/SwitchBtn";
+import { useContext } from "react";
+import { ViewContext } from "../../context/viewContext";
+
 function Home() {
-  return <div>Home</div>;
+  const { isProviderView } = useContext(ViewContext);
+
+  return (
+    <div className="">
+      <SwitchBtn />
+      <div className="mt-4">
+        {isProviderView ? (
+          <div>
+            <h3 className="text-center">學生的內容</h3>
+          </div>
+        ) : (
+          <div>
+            <h3 className="text-center">老師</h3>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
