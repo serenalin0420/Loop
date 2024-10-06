f2f07c6b0fbae7b04d19f0506e92736bc74c0e89
Serena <serenalin0420@gmail.com>
2024-10-06 17:17:27 +0800

    style: rwd for header and footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,13 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="h-[60px] bg-stone-50 py-4 font-semibold text-indian-khaki-600 shadow-inner sm:h-auto sm:py-6">
+      <div className="text-center">
+        <p className="text-sm sm:text-base">
+          &copy; 2024 Skill Exchang Platform - Loop. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,12 @@ import { Link, useNavigate } from "react-router-dom";
 import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState, useContext } from "react";
 import logo from "./logo.svg";
-import { BookBookmark, Bell, ChatCircleDots } from "@phosphor-icons/react";
+import {
+  BookBookmark,
+  Bell,
+  ChatCircleDots,
+  House,
+} from "@phosphor-icons/react";
 import { UserContext } from "../../context/userContext";
 import PropTypes from "prop-types";
 import { useLocation } from "react-router-dom";
@@ -82,16 +87,85 @@ function Header({ onNotificationClick, hasUnreadNotifications }) {
   };
 
   return (
-    <div className="fixed left-0 top-0 z-20 flex h-[60px] w-full transform items-center bg-white shadow-md transition-all duration-500">
-      <Link to="/" className="mx-8">
-        <img src={logo} alt="Loop" className="w-24" />
-      </Link>
-      {isLoggedIn ? (
-        <>
+    <>
+      <header className="fixed left-0 top-0 z-20 flex h-[60px] w-full transform items-center justify-center bg-white shadow-md transition-all duration-500 sm:justify-start">
+        <Link to="/" className="relative mx-8 mt-1">
+          <img src={logo} alt="Loop" className="w-24" />
+        </Link>
+        {isLoggedIn ? (
+          <>
+            <div className="hidden items-center sm:ml-auto sm:flex">
+              {user && (
+                <Link
+                  to={`/learning-portfolio/${user.uid}`}
+                  className="mr-1 flex flex-col items-center gap-[2px] p-2 text-[10px] text-indian-khaki-800"
+                >
+                  <BookBookmark
+                    className="size-7"
+                    weight={calendarWeight}
+                    onMouseEnter={() => setCalendarWeight("fill")}
+                    onMouseLeave={() => setCalendarWeight("regular")}
+                  />
+                  學習歷程表
+                </Link>
+              )}
+              <Link
+                to={topChatId ? `/chat/${topChatId}` : "/chat"}
+                className="mr-1 flex flex-col items-center gap-[2px] p-2 text-[10px] text-indian-khaki-800"
+              >
+                <ChatCircleDots className="size-7" />
+                訊息
+              </Link>
+              <button
+                className="relative mr-1 flex flex-col items-center gap-[2px] p-2 text-[10px] text-indian-khaki-800"
+                onClick={handleNotificationClick}
+              >
+                <Bell className="size-7" weight={bellWeight} />
+                {hasUnreadNotifications && (
+                  <span className="absolute right-2 top-[6px] h-3 w-3 rounded-full bg-red-400"></span>
+                )}
+                通知
+              </button>
+              <Link to="/profile" className="mx-2">
+                <img
+                  src={user?.profile_picture}
+                  className="mr-[72px] size-10 rounded-full bg-red-100 object-cover object-center"
+                  alt="author"
+                />
+              </Link>
+            </div>
+            <button
+              onClick={handleLogout}
+              className="absolute right-2 top-3 rounded-full px-4 py-2 transition hover:bg-indian-khaki-300 hover:text-white active:bg-indian-khaki-300"
+            >
+              登出
+            </button>
+          </>
+        ) : (
+          <button
+            onClick={handleLogin}
+            className="absolute right-2 top-3 rounded-full px-4 py-2 transition hover:bg-indian-khaki-300 hover:text-white active:bg-indian-khaki-300"
+          >
+            登入
+          </button>
+        )}
+      </header>
+
+      {/* Header for mobile */}
+
+      {isLoggedIn && (
+        <div className="fixed bottom-0 left-0 z-20 flex h-[60px] w-full transform items-center justify-between bg-white px-4 shadow-[0_-1px_3px_rgba(0,0,0,0.1)] transition-all duration-500 xs:px-6 sm:hidden">
+          <Link
+            to="/"
+            className="mr-1 flex flex-col items-center gap-[2px] p-2 text-[10px] text-indian-khaki-800"
+          >
+            <House className="size-7" />
+            首頁
+          </Link>
           {user && (
             <Link
               to={`/learning-portfolio/${user.uid}`}
-              className="text-indian-khaki-800 ml-auto mr-1 flex flex-col items-center gap-[2px] p-2 text-[10px]"
+              className="mr-1 flex flex-col items-center gap-[2px] p-2 text-[10px] text-indian-khaki-800"
             >
               <BookBookmark
                 className="size-7"
@@ -104,13 +178,13 @@ function Header({ onNotificationClick, hasUnreadNotifications }) {
           )}
           <Link
             to={topChatId ? `/chat/${topChatId}` : "/chat"}
-            className="text-indian-khaki-800 mr-1 flex flex-col items-center gap-[2px] p-2 text-[10px]"
+            className="mr-1 flex flex-col items-center gap-[2px] p-2 text-[10px] text-indian-khaki-800"
           >
             <ChatCircleDots className="size-7" />
             訊息
           </Link>
           <button
-            className="text-indian-khaki-800 relative mr-1 flex flex-col items-center gap-[2px] p-2 text-[10px]"
+            className="relative mr-1 flex flex-col items-center gap-[2px] p-2 text-[10px] text-indian-khaki-800"
             onClick={handleNotificationClick}
           >
             <Bell className="size-7" weight={bellWeight} />
@@ -126,22 +200,9 @@ function Header({ onNotificationClick, hasUnreadNotifications }) {
               alt="author"
             />
           </Link>
-          <button
-            onClick={handleLogout}
-            className="hover:bg-indian-khaki-300 active:bg-indian-khaki-300 mr-4 mt-1 rounded-full px-4 py-2 transition hover:text-white"
-          >
-            登出
-          </button>
-        </>
-      ) : (
-        <button
-          onClick={handleLogin}
-          className="hover:bg-indian-khaki-300 active:bg-indian-khaki-300 ml-auto mr-4 mt-1 rounded-full px-4 py-2 transition hover:text-white"
-        >
-          登入
-        </button>
+        </div>
       )}
-    </div>
+    </>
   );
 }
 
