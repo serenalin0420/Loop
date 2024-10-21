a9994394457f84b88506d60255746be776ed7ae5
Serena <serenalin0420@gmail.com>
2024-10-21 15:02:30 +0800

    feat: add Mock data in Login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -8,6 +8,11 @@ import { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import logo from "../../components/Header/loop-logo.png";
 
+const mockData = {
+  email: "admin@mail.com",
+  password: "admin123",
+};
+
 export default function Login() {
   const navigate = useNavigate();
   const location = useLocation();
@@ -87,110 +92,118 @@ export default function Login() {
     }
   }
 
-  return (
-    <div className="bg-[#fcf8f3] sm:p-4">
-      <div className="flex min-h-screen flex-col bg-white bg-mobile-login-bg bg-cover bg-center bg-no-repeat px-4 sm:rounded-xl sm:bg-login-bg md:flex-row md:justify-between md:px-8 lg:px-10 xl:px-16">
-        <div className="order-3 flex justify-center gap-20 md:order-1 md:ml-4 md:mr-28 md:mt-36 md:flex-col md:justify-start md:gap-8 lg:mr-44">
-          <button
-            className={`text-nowrap px-3 py-2 text-lg font-semibold md:px-4 md:text-xl md:tracking-wide ${
-              isRegistering
-                ? "text-indian-khaki-400"
-                : "rounded border-b-4 border-cerulean-400 text-cerulean-900"
-            } hidden md:block`}
-            onClick={() => setIsRegistering(false)}
-          >
-            登入
-          </button>
-          <button
-            className={`text-nowrap px-2 py-2 text-lg font-semibold md:px-4 md:text-xl md:tracking-wide ${
-              isRegistering
-                ? "rounded border-b-4 border-cerulean-400 text-cerulean-900"
-                : "text-indian-khaki-400"
-            } hidden md:block`}
-            onClick={() => setIsRegistering(true)}
-          >
-            註冊
-          </button>
-          <button
-            className="flex cursor-pointer text-nowrap px-3 py-2 md:hidden"
-            onClick={() => setIsRegistering(!isRegistering)}
-          >
-            <p className="mr-4 cursor-pointer">
-              {isRegistering ? "已有帳號嗎? " : "還沒有帳號嗎?"}
-            </p>
-            {isRegistering ? "登入" : "註冊 "}
-          </button>
-        </div>
-        <div className="mt-32 flex h-fit flex-col flex-wrap items-center py-4 sm:mt-52 md:order-2 md:w-2/5 md:py-6">
-          <Link to="/" className="mx-8 cursor-pointer">
-            <img src={logo} alt="Loop" className="w-44 md:w-56" />{" "}
-          </Link>
-          <h3 className="mt-2 text-center text-cerulean-950">
-            以<strong>代幣</strong>為中心將技能平等化，
-            <br />
-            透過教學獲得<strong className="text-neon-carrot-400">代幣</strong>
-            ，用
-            <strong className="text-cerulean-500">代幣</strong>換取學習！
-          </h3>
+  const handleFillMockData = () => {
+    setEmail(mockData.email);
+    setPassword(mockData.password);
+  };
 
-          <form className="mt-4 flex-col md:mt-8" onSubmit={onSubmit}>
-            {isRegistering && (
-              <fieldset>
-                <input
-                  value={username}
-                  placeholder="請輸入你的名字或暱稱~"
-                  onChange={(e) => setUsername(e.target.value)}
-                  className="mb-3 mt-1 flex min-w-72 rounded-md border border-stone-200 bg-white px-3 py-2 focus:outline-indian-khaki-400"
-                  type="text"
-                  required
-                />
-              </fieldset>
-            )}
+  return (
+    <div className="flex min-h-screen flex-col bg-white bg-mobile-login-bg bg-cover bg-center bg-no-repeat px-4 sm:bg-login-bg sm:p-4 md:flex-row md:justify-between md:px-8 lg:px-10 xl:px-16">
+      <div className="order-3 flex justify-center gap-20 md:order-1 md:ml-4 md:mr-28 md:mt-36 md:flex-col md:justify-start md:gap-8 lg:mr-44">
+        <button
+          className={`text-nowrap px-3 py-2 text-lg font-semibold md:px-4 md:text-xl md:tracking-wide ${
+            isRegistering
+              ? "text-indian-khaki-400"
+              : "rounded border-b-4 border-cerulean-400 text-cerulean-900"
+          } hidden md:block`}
+          onClick={() => setIsRegistering(false)}
+        >
+          登入
+        </button>
+        <button
+          className={`text-nowrap px-2 py-2 text-lg font-semibold md:px-4 md:text-xl md:tracking-wide ${
+            isRegistering
+              ? "rounded border-b-4 border-cerulean-400 text-cerulean-900"
+              : "text-indian-khaki-400"
+          } hidden md:block`}
+          onClick={() => setIsRegistering(true)}
+        >
+          註冊
+        </button>
+        <button
+          className="flex cursor-pointer text-nowrap px-3 py-2 md:hidden"
+          onClick={() => setIsRegistering(!isRegistering)}
+        >
+          <p className="mr-4 cursor-pointer">
+            {isRegistering ? "已有帳號嗎? " : "還沒有帳號嗎?"}
+          </p>
+          {isRegistering ? "登入" : "註冊 "}
+        </button>
+      </div>
+      <div className="mt-32 flex h-fit flex-col flex-wrap items-center py-4 sm:mt-28 md:order-2 md:w-2/5 md:py-6">
+        <Link to="/" className="mx-8 cursor-pointer">
+          <img src={logo} alt="Loop" className="w-44 md:w-56" />
+        </Link>
+        <h3 className="mt-2 text-center text-cerulean-950">
+          以<strong>代幣</strong>為中心將技能平等化，
+          <br />
+          透過教學獲得<strong className="text-neon-carrot-400">代幣</strong>
+          ，用
+          <strong className="text-cerulean-500">代幣</strong>換取學習！
+        </h3>
+
+        <form className="mt-4 flex-col md:mt-8" onSubmit={onSubmit}>
+          {isRegistering && (
             <fieldset>
               <input
-                value={email}
-                placeholder="請輸入電子信箱"
-                onChange={(e) => setEmail(e.target.value)}
-                className="mb-3 mt-1 flex min-w-72 rounded-md border border-stone-200 bg-white px-3 py-2 caret-indian-khaki-700 focus:outline-indian-khaki-400"
-                type="email"
+                value={username}
+                placeholder="請輸入你的名字或暱稱~"
+                onChange={(e) => setUsername(e.target.value)}
+                className="mb-3 mt-1 flex min-w-72 rounded-md border border-stone-200 bg-white px-3 py-2 focus:outline-indian-khaki-400"
+                type="text"
                 required
               />
             </fieldset>
+          )}
+          <fieldset>
+            <input
+              value={email}
+              placeholder="請輸入電子信箱"
+              onChange={(e) => setEmail(e.target.value)}
+              className="mb-3 mt-1 flex min-w-72 rounded-md border border-stone-200 bg-white px-3 py-2 caret-indian-khaki-700 focus:outline-indian-khaki-400"
+              type="email"
+              required
+            />
+          </fieldset>
+          <fieldset>
+            <input
+              value={password}
+              placeholder="請輸入密碼"
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              className="mb-3 mt-1 flex min-w-72 rounded-md border border-stone-200 bg-white px-3 py-2 caret-indian-khaki-700 focus:outline-indian-khaki-400"
+              required
+            />
+          </fieldset>
+          {isRegistering && (
             <fieldset>
               <input
-                value={password}
-                placeholder="請輸入密碼"
-                onChange={(e) => setPassword(e.target.value)}
+                value={confirmPassword}
+                placeholder="請再輸入一次密碼"
+                onChange={(e) => setConfirmPassword(e.target.value)}
                 type="password"
                 className="mb-3 mt-1 flex min-w-72 rounded-md border border-stone-200 bg-white px-3 py-2 caret-indian-khaki-700 focus:outline-indian-khaki-400"
                 required
               />
             </fieldset>
-            {isRegistering && (
-              <fieldset>
-                <input
-                  value={confirmPassword}
-                  placeholder="請再輸入一次密碼"
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                  type="password"
-                  className="mb-3 mt-1 flex min-w-72 rounded-md border border-stone-200 bg-white px-3 py-2 caret-indian-khaki-700 focus:outline-indian-khaki-400"
-                  required
-                />
-              </fieldset>
-            )}
-            {error && (
-              <p className="text-center text-xs text-red-400">{error}</p>
-            )}
-            <button className="mt-2 w-full rounded-md bg-indian-khaki-400 px-5 py-2 tracking-widest text-white hover:bg-[#8a7b60] active:bg-[#8a7b60] md:mt-6">
-              {isRegistering ? "註冊" : "登入"}
-            </button>
-          </form>
-        </div>
-        <h3 className="hidden md:order-2 md:mb-12 md:mr-4 md:mt-auto md:block md:text-center md:font-semibold md:text-indian-khaki-400">
-          <span className="block lg:inline">讓你的技能有效地</span>
-          <span className="block lg:inline">進入學習的循環</span>
-        </h3>
+          )}
+          {error && <p className="text-center text-xs text-red-400">{error}</p>}
+          <button className="mt-2 w-full rounded-md bg-indian-khaki-400 px-5 py-2 tracking-widest text-white hover:bg-[#8a7b60] active:bg-[#8a7b60] md:mt-6">
+            {isRegistering ? "註冊" : "登入"}
+          </button>
+
+          <button
+            onClick={handleFillMockData}
+            className="mt-4 w-full rounded-md bg-indian-khaki-100 px-5 py-2 text-indian-khaki-800 hover:bg-indian-khaki-200"
+          >
+            使用 訪客帳號 登入
+          </button>
+        </form>
       </div>
+      <h3 className="hidden md:order-2 md:mb-12 md:mr-4 md:mt-auto md:block md:text-center md:font-semibold md:text-indian-khaki-400">
+        <span className="block lg:inline">讓你的技能有效地</span>
+        <span className="block lg:inline">進入學習的循環</span>
+      </h3>
     </div>
   );
 }
